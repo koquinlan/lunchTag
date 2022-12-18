c841697c011b2b15a19dd8df8a573143c2d2987f
koquinlan <kyle.quinlan@colorado.edu>
2022-12-17 22:12:05 -0500

    Debug on

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
 
