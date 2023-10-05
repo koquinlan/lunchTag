513d76d79ef962ec6a7aacb44846382036bac2cd
koquinlan <kyle.quinlan@colorado.edu>
2023-10-05 11:06:20 -0600

    Added deactivation command

---
+++ core/management/commands/deactivate.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from core.models import Profile, Pairing
+
+
+class DeactivateUsers(BaseCommand):
+    help = "Sets all users to deactivated - useful for end of a semester"
+
+    # define logic of command
+    def handle(self, *args, **options):
+        for profile in Profile.objects.all():
+            profile.active = True
+            profile.crush = None
+            profile.has_tag_pairing = False
+            profile.tag_pairing = None
+            profile.tag_pairing2 = None
+
+            profile.save()
+        
+        Pairing.objects.all().delete()
